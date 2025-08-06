51b8016769ab4378ea3a5deefa8e341e9af7c6fd
Sameer Patwari <sameer.patwari@unplatforms.com>
2025-08-06 17:26:25 +0530

    new changes

--- src/env.js
+++ src/env.js
@@ -3,11 +3,11 @@ window.env = {
     USERNAME: "user@unplatforms.com",
     PASSWORD: "up@2025",
     PDFS: [
-      { title: "Claim 2353933", path: "/public/pdfs/claim 2353933.pdf" },
-      { title: "Claim 2382358", path: "/public/pdfs/claim 2382358.pdf" },
-      { title: "DSG 2404792", path: "/public/pdfs/DSG 2404792.pdf" },
-      { title: "DSG 2404822", path: "/public/pdfs/DSG 2404822.pdf" },
-      { title: "SKM-450i25080512520", path: "/public/pdfs/SKM-450i25080512520.pdf" },
-      { title: "SKM-450i25080513100", path: "/public/pdfs/SKM-450i25080513100.pdf" }
+      { title: "Claim 2353933", path: "/pdfs/claim 2353933.pdf" },
+      { title: "Claim 2382358", path: "/pdfs/claim 2382358.pdf" },
+      { title: "DSG 2404792", path: "/pdfs/DSG 2404792.pdf" },
+      { title: "DSG 2404822", path: "/pdfs/DSG 2404822.pdf" },
+      { title: "SKM-450i25080512520", path: "/pdfs/SKM-450i25080512520.pdf" },
+      { title: "SKM-450i25080513100", path: "/pdfs/SKM-450i25080513100.pdf" }
     ]
   };
